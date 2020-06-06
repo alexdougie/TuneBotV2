05e3e486907f5713e54ed5f765c931ebb70bcc50
Alex <Alex@DESKTOP-K1R9N8U>
2020-06-06 11:44:28 +0100

    Add YouTube searching

--- TuneBot/Modules/TrackModule.cs
+++ TuneBot/Modules/TrackModule.cs
@@ -1,4 +1,7 @@
-﻿using Discord.Commands;
+﻿#nullable enable
+using Discord.Commands;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
 using Microsoft.Extensions.Configuration;
 using SpotifyAPI.Web;
 using SpotifyAPI.Web.Auth;
@@ -18,6 +21,15 @@ namespace TuneBot.Modules
         [Summary("Gets a track from Spotify and YouTube.")]
         public async Task GetTrack([Remainder]string details)
         {
+            var spotifyResult = await SearchSpotify(details);
+
+            var youTubeResult = await SearchYouTube(details);
+
+            await ReplyAsync(spotifyResult + "\n" + youTubeResult);
+        }
+
+        private async Task<string> SearchSpotify(string details)
+        {
             // todo: singleton spotify object
             var credentialsAuth = new CredentialsAuth(
                 config["SpotifyId"],
@@ -30,10 +42,40 @@ namespace TuneBot.Modules
                 TokenType = token.TokenType
             };
 
-            var track = await spotify.SearchItemsAsync(details, SearchType.Track, 1);
+            var searchResult = await spotify.SearchItemsAsync(details, SearchType.Track, 1);
 
-            //todo: nullable reference types!
-            await ReplyAsync(track.Tracks.Items.First().ExternUrls["spotify"]);
+            var track = searchResult.Tracks.Items.First();
+
+            var result =
+                track != null ?
+                    track.ExternUrls["spotify"] :
+                    "No Spotify result found";
+
+            return result;
+        }
+
+        private async Task<string> SearchYouTube(string details)
+        {
+            var youtubeService = new YouTubeService(
+                new BaseClientService.Initializer()
+                {
+                    ApiKey = config["YouTubeKey"]
+                });
+
+            var searchRequest = youtubeService.Search.List("snippet");
+            searchRequest.Q = details;
+            searchRequest.MaxResults = 5;
+
+            var response = await searchRequest.ExecuteAsync();
+
+            var searchResult = response.Items.Where(i => i.Id.Kind == "youtube#video").First();
+
+            var result =
+                searchResult != null ?
+                    $"https://youtu.be/{searchResult.Id.VideoId}" :
+                    "No YouTube result found";
+
+            return result;
         }
     }
 }
--- TuneBot/Startup.cs
+++ TuneBot/Startup.cs
@@ -26,8 +26,6 @@ namespace TuneBot
 
             await startup.RunAsync();
         }
-        
-
 
         private async Task RunAsync()
         {
